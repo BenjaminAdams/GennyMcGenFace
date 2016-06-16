977a5e5a40ec6961dc1dda5f2b16e551164916b7
Benjamin Adams <ben@benadams.co>
2016-06-16 17:43:44 -0500

    depth working

--- GennyMcGenFace/CodeGenerator.cs
+++ GennyMcGenFace/CodeGenerator.cs
@@ -16,30 +16,31 @@ namespace GennyMcGenFace.GennyMcGenFace
 {
     public static class CodeGenerator
     {
-        private static string _leadingSpaces = "   ";
+        // private static string GetSpaces(depth) = "   ";
 
         public static string GenerateClass(CodeClass selectedClass)
         {
             var str = string.Format("var obj = new {0}() {{\r\n", selectedClass.FullName);
-            str += IterateMembers(selectedClass.Members);
+            str += IterateMembers(selectedClass.Members, 0);
             str += "\r\n};";
             return str;
         }
 
-        private static string IterateMembers(CodeElements members)
+        private static string IterateMembers(CodeElements members, int depth)
         {
+            depth++;
             var str = "";
             foreach (CodeProperty member in members.OfType<CodeProperty>())
             {
                 if (CodeDiscoverer.IsValidPublicMember((CodeElement)member) == false) continue;
-                str += GetParamValue(member);
+                str += GetParamValue(member, depth);
             }
 
             return str;
         }
 
         //this will help http://stackoverflow.com/questions/6303425/auto-generate-properties-when-creating-object
-        private static string ParseObjects(CodeProperty member)
+        private static string ParseObjects(CodeProperty member, int depth)
         {
             if (member.Type.CodeType.Name == "List" || member.Type.CodeType.Name == "ICollection" || member.Type.CodeType.Name == "IList" || member.Type.CodeType.Name == "IEnumerable")
             {
@@ -47,18 +48,18 @@ namespace GennyMcGenFace.GennyMcGenFace
                 var baseType = member.ProjectItem.ContainingProject.CodeModel.CodeTypeFromFullName(GetBaseTypeFromList(member.Type.AsFullName));
                 var typeFullName = string.Format("List<{0}>", baseType.FullName); //and alternative to this could be member.Type.AsFullName
 
-                var objAsStr = string.Format("{0}new {1}() {{\r\n{2}\r\n{0}}},\r\n", _leadingSpaces, baseType.FullName, IterateMembers(baseType.Members));
+                var objAsStr = string.Format("{0}new {1}() {{\r\n{2}{0}}},\r\n", GetSpaces(depth + 1), baseType.FullName, IterateMembers(baseType.Members, depth));
 
-                return string.Format("{0}{1} = new {2}() {{\r\n{3}}},\r\n", _leadingSpaces, member.Name, typeFullName, objAsStr);
+                return string.Format("{0}{1} = new {2}() {{\r\n{3}{0}}},\r\n", GetSpaces(depth), member.Name, typeFullName, objAsStr);
             }
             else
             {
                 //plain object
-                return string.Format("{0}{1} = new {2}() {{\r\n{3}{0}}},\r\n", _leadingSpaces, member.Name, member.Type.AsFullName, IterateMembers(member.Type.CodeType.Members));
+                return string.Format("{0}{1} = new {2}() {{\r\n{3}{0}}},\r\n", GetSpaces(depth), member.Name, member.Type.AsFullName, IterateMembers(member.Type.CodeType.Members, depth));
             }
         }
 
-        private static string GetParamValue(CodeProperty member)
+        private static string GetParamValue(CodeProperty member, int depth)
         {
             var rand = new Random();
 
@@ -69,56 +70,56 @@ namespace GennyMcGenFace.GennyMcGenFace
                 var month = DateTime.Now.Month;
                 var day = DateTime.Now.Day;
                 var dateAsStr = string.Format("new DateTime({0}, {1}, {2})", year, month, day);
-                return string.Format("{0}{1} = {2},\r\n", _leadingSpaces, member.Name, dateAsStr);
+                return string.Format("{0}{1} = {2},\r\n", GetSpaces(depth), member.Name, dateAsStr);
             }
             else if (member.Type.TypeKind == vsCMTypeRef.vsCMTypeRefCodeType)
             {
                 //defined types/objects we have created
-                return ParseObjects(member);
+                return ParseObjects(member, depth);
             }
             else if (member.Type.TypeKind == vsCMTypeRef.vsCMTypeRefString)
             {
                 //string
-                return _leadingSpaces + member.Name + " = \"yay\",\r\n";
+                return GetSpaces(depth) + member.Name + " = \"yay\",\r\n";
             }
             else if (member.Type.TypeKind == vsCMTypeRef.vsCMTypeRefChar)
             {
                 //char
-                return _leadingSpaces + member.Name + " = 'a',\r\n";
+                return GetSpaces(depth) + member.Name + " = 'a',\r\n";
             }
             else if (member.Type.TypeKind == vsCMTypeRef.vsCMTypeRefBool)
             {
                 //bool
                 var val = rand.Next(0, 1) == 1 ? "true" : "false";
-                return _leadingSpaces + member.Name + " = " + val + ",\r\n";
+                return GetSpaces(depth) + member.Name + " = " + val + ",\r\n";
             }
             else if (member.Type.TypeKind == vsCMTypeRef.vsCMTypeRefDecimal || member.Type.TypeKind == vsCMTypeRef.vsCMTypeRefDouble || member.Type.TypeKind == vsCMTypeRef.vsCMTypeRefFloat || member.Type.TypeKind == vsCMTypeRef.vsCMTypeRefInt || member.Type.TypeKind == vsCMTypeRef.vsCMTypeRefLong)
             {
                 //numbers (except short)
                 var val = rand.Next(0, 999999999);
 
-                return string.Format("{0}{1} = {2},\r\n", _leadingSpaces, member.Name, val);
+                return string.Format("{0}{1} = {2},\r\n", GetSpaces(depth), member.Name, val);
             }
             else if (member.Type.TypeKind == vsCMTypeRef.vsCMTypeRefShort)
             {
                 //short
                 var val = rand.Next(0, 9999);
-                return string.Format("{0}{1} = {2},\r\n", _leadingSpaces, member.Name, val);
+                return string.Format("{0}{1} = {2},\r\n", GetSpaces(depth), member.Name, val);
             }
             else if (member.Type.TypeKind == vsCMTypeRef.vsCMTypeRefArray)
             {
                 //array
-                return _leadingSpaces + member.Name + " = \"yay\",\r\n";
+                return GetSpaces(depth) + member.Name + " = \"yay\",\r\n";
             }
             else if (member.Type.TypeKind == vsCMTypeRef.vsCMTypeRefByte)
             {
                 //byte
-                return _leadingSpaces + member.Name + " = new Byte(),\r\n";
+                return GetSpaces(depth) + member.Name + " = new Byte(),\r\n";
             }
             else if (member.Type.TypeKind == vsCMTypeRef.vsCMTypeRefObject)
             {
                 //object
-                return _leadingSpaces + member.Name + " = new Object(),\r\n";
+                return GetSpaces(depth) + member.Name + " = new Object(),\r\n";
             }
             else
             {
@@ -141,7 +142,7 @@ namespace GennyMcGenFace.GennyMcGenFace
         private static string GetSpaces(int depth)
         {
             var spaces = "";
-            for (var i = 0; i > depth; i++)
+            for (var i = 0; i < depth; i++)
             {
                 spaces += "   ";
             }
